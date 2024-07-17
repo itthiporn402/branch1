21f4e2100d09d0ff213b01974144b84ee81a2705
itthiporn402 <itthiporn7234@gmail.com>
2024-07-17 14:31:12 +0700

    Add template part

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="q-pa-md" style="max-width: 400px">
-
     <q-form
       @submit="onSubmit"
       @reset="onReset"
@@ -14,7 +13,6 @@
         lazy-rules
         :rules="[ val => val && val.length > 0 || 'Please type something']"
       />
-
       <q-input
         filled
         type="number"
@@ -26,15 +24,12 @@
           val => val > 0 && val < 100 || 'Please type a real age'
         ]"
       />
-
       <q-toggle v-model="accept" label="I accept the license and terms" />
-
       <div>
         <q-btn label="Submit" type="submit" color="primary"/>
         <q-btn label="Reset" type="reset" color="primary" flat class="q-ml-sm" />
       </div>
     </q-form>
-
   </div>
 </template>
 
